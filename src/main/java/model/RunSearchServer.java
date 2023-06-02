a550973aa4475248a437801f53439f54c9f0eba8
Asher Lagemi <asherlagemi@gmail.com>
2023-06-02 19:22:04 +0300

    Changes to structure and added classes

--- src/main/java/model/RunSearchServer.java
+++ src/main/java/model/RunSearchServer.java
@@ -10,6 +10,7 @@ import java.util.Scanner;
 public class RunSearchServer extends RunServer{
 
     public static void main(String[] args) {
+
         Random r = new Random();
         int port = 6000 + r.nextInt(1000);
         System.out.println("port: " + port);
